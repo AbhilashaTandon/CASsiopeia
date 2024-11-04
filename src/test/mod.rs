1e3b44e3c67f6439adb7ae5f1b7b8ad3a14d764b
Abhilasha Tandon <abhilashatandon167@gmail.com>
2024-11-03 21:00:43 -0500

    Modularized method for testing scanner output

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -15,123 +15,120 @@ pub(crate) mod test {
     ) -> TokenItem {
         return TokenItem::Token {
             token_name,
-            // token_text: String::from(token_text),
             token_value,
         };
     }
 
-    #[test]
-    fn variable_declarations() {
-        let computed_tokens: Tokenization = tokenize(String::from("x = 2"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Equal, None),
-            make_token(Int, Some(Value::Int(2))),
-        ];
+    fn run_test(line_of_code: &str, desired_tokens: Vec<TokenItem>, desired_errors: Vec<CASError>) {
+        let computed_tokens: Tokenization = tokenize(String::from(line_of_code));
+
         let desired_output: Tokenization = Tokenization {
             tokens: desired_tokens,
-            errors: vec![],
+            errors: desired_errors,
         };
         assert_eq!(computed_tokens, desired_output);
+    }
+
+    #[test]
+    fn variable_declarations() {
+        run_test(
+            "x = 2",
+            vec![
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Equal, None),
+                make_token(Int, Some(Value::Int(2))),
+            ],
+            vec![],
+        );
     }
 
     #[test]
     fn function_declarations() {
-        let computed_tokens: Tokenization = tokenize(String::from("f(x,y) = 2 * x + 3 * y"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("f")))),
-            make_token(LeftParen, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Comma, None),
-            make_token(Name, Some(Value::String(String::from("y")))),
-            make_token(RightParen, None),
-            make_token(Equal, None),
-            make_token(Int, Some(Value::Int(2))),
-            make_token(Mult, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Add, None),
-            make_token(Int, Some(Value::Int(3))),
-            make_token(Mult, None),
-            make_token(Name, Some(Value::String(String::from("y")))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "f(x,y) = 2 * x + 3 * y",
+            vec![
+                make_token(Name, Some(Value::String(String::from("f")))),
+                make_token(LeftParen, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Comma, None),
+                make_token(Name, Some(Value::String(String::from("y")))),
+                make_token(RightParen, None),
+                make_token(Equal, None),
+                make_token(Int, Some(Value::Int(2))),
+                make_token(Mult, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Add, None),
+                make_token(Int, Some(Value::Int(3))),
+                make_token(Mult, None),
+                make_token(Name, Some(Value::String(String::from("y")))),
+            ],
+            vec![],
+        );
     }
 
     #[test]
     fn keywords() {
-        let computed_tokens: Tokenization = tokenize(String::from("calc 3 * x - 5"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Calc, None),
-            make_token(Int, Some(Value::Int(3))),
-            make_token(Mult, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Sub, None),
-            make_token(Int, Some(Value::Int(5))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "calc 3 * x - 5",
+            vec![
+                make_token(Calc, None),
+                make_token(Int, Some(Value::Int(3))),
+                make_token(Mult, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Sub, None),
+                make_token(Int, Some(Value::Int(5))),
+            ],
+            vec![],
+        );
 
-        let computed_tokens: Tokenization = tokenize(String::from("sim 3 * x - 5"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Sim, None),
-            make_token(Int, Some(Value::Int(3))),
-            make_token(Mult, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Sub, None),
-            make_token(Int, Some(Value::Int(5))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "sim 3 * x - 5",
+            vec![
+                make_token(Sim, None),
+                make_token(Int, Some(Value::Int(3))),
+                make_token(Mult, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Sub, None),
+                make_token(Int, Some(Value::Int(5))),
+            ],
+            vec![],
+        );
 
-        let computed_tokens: Tokenization = tokenize(String::from("der 3 * x - 5, x"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Der, None),
-            make_token(Int, Some(Value::Int(3))),
-            make_token(Mult, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Sub, None),
-            make_token(Int, Some(Value::Int(5))),
-            make_token(Comma, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "der 3 * x - 5, x",
+            vec![
+                make_token(Der, None),
+                make_token(Int, Some(Value::Int(3))),
+                make_token(Mult, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Sub, None),
+                make_token(Int, Some(Value::Int(5))),
+                make_token(Comma, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+            ],
+            vec![],
+        );
     }
 
     #[test]
     fn dashes() {
-        let computed_tokens: Tokenization = tokenize(String::from("x-y_z = -5 + 3 - 2 - -4"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("x-y_z")))),
-            make_token(Equal, None),
-            make_token(Sub, None),
-            make_token(Int, Some(Value::Int(5))),
-            make_token(Add, None),
-            make_token(Int, Some(Value::Int(3))),
-            make_token(Sub, None),
-            make_token(Int, Some(Value::Int(2))),
-            make_token(Sub, None),
-            make_token(Sub, None),
-            make_token(Int, Some(Value::Int(4))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "x-y_z = -5 + 3 - 2 - -4",
+            vec![
+                make_token(Name, Some(Value::String(String::from("x-y_z")))),
+                make_token(Equal, None),
+                make_token(Sub, None),
+                make_token(Int, Some(Value::Int(5))),
+                make_token(Add, None),
+                make_token(Int, Some(Value::Int(3))),
+                make_token(Sub, None),
+                make_token(Int, Some(Value::Int(2))),
+                make_token(Sub, None),
+                make_token(Sub, None),
+                make_token(Int, Some(Value::Int(4))),
+            ],
+            vec![],
+        );
     }
 
     #[test]
@@ -140,90 +137,74 @@ pub(crate) mod test {
         // let mut tokens: Vec<TokenItem> = vec![];
         // process_line("_x = 2", &mut tokens, 0);
 
-        let computed_tokens = tokenize(String::from("_x = 2"));
+        run_test(
+            "_x = 2",
+            vec![
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Equal, None),
+                make_token(Int, Some(Value::Int(2))),
+            ],
+            vec![CASError {
+                line_pos: 1,
+                kind: crate::error::error::CASErrorKind::MalformedVariableName,
+            }],
+        );
 
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Equal, None),
-            make_token(Int, Some(Value::Int(2))),
-        ];
-        let desired_errors: Vec<CASError> = vec![CASError {
-            line_pos: 1,
-            kind: crate::error::error::CASErrorKind::MalformedVariableName,
-        }];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: desired_errors,
-        };
-        assert_eq!(computed_tokens, desired_output);
-
-        let computed_tokens: Tokenization = tokenize(String::from("-x = 2"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Sub, None),
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Equal, None),
-            make_token(Int, Some(Value::Int(2))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "-x = 2",
+            vec![
+                make_token(Sub, None),
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Equal, None),
+                make_token(Int, Some(Value::Int(2))),
+            ],
+            vec![],
+        );
     }
 
     #[test]
     fn floats() {
-        //variable names can't begin w underscore
-        let computed_tokens: Tokenization = tokenize(String::from("x = 3.3343"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("x")))),
-            make_token(Equal, None),
-            make_token(Float, Some(Value::Float(3.3343))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "x = 3.3343",
+            vec![
+                make_token(Name, Some(Value::String(String::from("x")))),
+                make_token(Equal, None),
+                make_token(Float, Some(Value::Float(3.3343))),
+            ],
+            vec![],
+        );
 
-        let computed_tokens: Tokenization = tokenize(String::from("y = -102342."));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("y")))),
-            make_token(Equal, None),
-            make_token(Sub, None),
-            make_token(Float, Some(Value::Float(102342.0))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "y = -102342.",
+            vec![
+                make_token(Name, Some(Value::String(String::from("y")))),
+                make_token(Equal, None),
+                make_token(Sub, None),
+                make_token(Float, Some(Value::Float(102342.0))),
+            ],
+            vec![],
+        );
 
-        let computed_tokens: Tokenization = tokenize(String::from("y = .102342"));
-        let desired_tokens: Vec<TokenItem> = vec![
-            make_token(Name, Some(Value::String(String::from("y")))),
-            make_token(Equal, None),
-            make_token(Float, Some(Value::Float(0.102342))),
-        ];
-        let desired_output: Tokenization = Tokenization {
-            tokens: desired_tokens,
-            errors: vec![],
-        };
-        assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "y = .102342",
+            vec![
+                make_token(Name, Some(Value::String(String::from("y")))),
+                make_token(Equal, None),
+                make_token(Float, Some(Value::Float(0.102342))),
+            ],
+            vec![],
+        );
 
-        // let computed_tokens: Tokenization = tokenize(String::from("y = .10.2342"));
-        // let desired_tokens: Vec<TokenItem> = vec![
-        //     make_token(Name, Some(Value::String(String::from("y")))),
-        //     make_token(Equal, None),
-        //     TokenItem::TokenError {
-        //         error_code: 1,
-        //         error_value: ".10.2342".to_string(),
-        //     },
-        // ];
-        // let desired_output: Tokenization = Tokenization {
-        //     tokens: desired_tokens,
-        //     error_code: 1,
-        // };
-        // assert_eq!(computed_tokens, desired_output);
+        run_test(
+            "y = .10.2342",
+            vec![
+                make_token(Name, Some(Value::String(String::from("y")))),
+                make_token(Equal, None),
+            ],
+            vec![CASError {
+                line_pos: 12,
+                kind: crate::error::error::CASErrorKind::MalformedNumericLiteral,
+            }],
+        );
     }
 }
